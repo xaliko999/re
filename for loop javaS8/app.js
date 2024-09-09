716c1ca39f4aca7a40bafe1c31bd99b87a01691a
xaliko999 <xalikomasixullayev@gmail.com>
2024-09-09 20:03:05 +0500

    dubl

---
+++ for loop javaS8/app.js
@@ -0,0 +1,12 @@
+let a = ['Akromjon','Abduqodr','ulugbrak','man']
+
+console.log(a);
+
+let d = confirm('array ayrilsinmi')
+
+if(d == true){
+    console.log( a = ['Akromjon','Abduqodr','ulugbrak']);
+}else{
+console.log( a = ['Akromjon','Abduqodr','ulugbrak','man']);
+
+}
